9b543b155d5c4f20654155e3e872dd40a9e82ad7
IlyaKulebyakin <kuler29@me.com>
2015-04-26 19:24:09 +0300

    method written, numerous refractions, is_bool decorator

--- StudentSite/math_backend.py
+++ StudentSite/math_backend.py
@@ -1,9 +1,8 @@
 from enum import Enum
-from .decorators import requires_int_or_bool
+from .decorators import requires_int_or_bool, is_bool
 import math
 
-
-# "[12,14,15,26]$[%10%3|<=|/10%3]@[/10%3|>=|%10%3]$[!@ ]$[and@...]"
+#   Example: "[12,14,15,26]$[%10%3| <= |/10%3]@[/10%3| >= |%10%3]$[ not @ ]$[ and @...]"
 #   Elements: 12, 14, 15, 26
 #   Triplets: ab%10%3 <= cd/10%3
 #             ab/10%3 >= cd%10%3
@@ -14,17 +13,29 @@ import math
 #   | between parts of each triplet
 #   Object to ^
 #   Object to human-readable
-#   ^ to object
+
 
 class UnaryRelation(Enum):
-    unary_not = 'not'
+    unary_not = ' not '
     unary_neutral = ' '
 
+    @is_bool
     def apply_unary_relation(self, argument):
+        """
+        :rtype:
+        :param argument: boolean to apply unary operation to
+        :return: argument itself
+        """
         if self == UnaryRelation.unary_not:
             return not argument
         else:
             return argument
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class BinaryRelation(Enum):
@@ -38,11 +49,16 @@ class BinaryRelation(Enum):
     logic_and = ' and '
     logic_xor = ' ^ '
 
-
     @requires_int_or_bool
     def apply_binary_relation(self, value1, value2):
         expression = str(value1) + self.value + str(value2)
         return eval(expression)
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class RelationElement:
@@ -62,14 +78,47 @@ class RelationTriplet:
         self.mod2 = mod2  # String
 
     def check(self, val1, val2):
+        print("gaw")
         elem1 = RelationElement(val1, self.mod1).get_modified()
         elem2 = RelationElement(val2, self.mod2).get_modified()
         return self.relation.apply_binary_relation(elem1, elem2)
 
+    def __str__(self):
+        res = self.mod1 + self.relation.value + self.mod2
+        return res
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class Task:
     def __init__(self, elements, triplets, triplet_modifiers, triplets_triplets_rel):
-        self.elements = elements  # Array on ints
-        self.triplets = triplets  # Array of RelationTriplet
-        self.triplet_modifiers = triplet_modifiers  # Array of UnaryRelation
-        self.triplets_triplets_rel = triplets_triplets_rel  # Array of BinaryRelation (logic)
+        """
+        :rtype: Task
+        :param elements: Array of ints
+        :param triplets: Array of RelationTriplet
+        :param triplet_modifiers: Array of UnaryRelation
+        :param triplets_triplets_rel: Array of BinaryRelation (logic)
+        :return: instance of Task
+        """
+        self.elements = elements
+        self.triplets = triplets
+        self.triplet_modifiers = triplet_modifiers
+        self.triplets_triplets_rel = triplets_triplets_rel
+
+    @classmethod
+    def from_string(cls, task_string):
+        """
+            :rtype : Task
+            :param task_string: String in "[12,14,15,26]$[%10%3| <= |/10%3]@[/10%3| >= |%10%3]$[ not @ ]$[ and @...]" format
+            :return: initialized instance of Task
+            """
+        task_elements = task_string.split('$')
+        elements = [int(int_str) for int_str in task_elements[0][1:-1].split(',')]
+        triplets = [RelationTriplet(*arg_tuple)
+                    for arg_tuple in [(rel_elements[0][1:], BinaryRelation(rel_elements[1]), rel_elements[2][:-1])
+                                      for rel_elements in [single_rel.split('|')
+                                                           for single_rel in task_elements[1].split('@')]]]
+        triplet_modifiers = [UnaryRelation(mod) for mod in task_elements[2][1:-1].split('@')]
+        triplets_triplets_rel = [BinaryRelation(mod) for mod in task_elements[3][1:-1].split('@')]
+        return Task(elements, triplets, triplet_modifiers, triplets_triplets_rel)
