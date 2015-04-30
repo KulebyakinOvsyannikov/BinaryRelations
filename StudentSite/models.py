d7c1cfbc3eff9e25975bb4b56717e1a10bd49dd9
IlyaKulebyakin <kuler29@me.com>
2015-04-30 12:20:52 +0300

    initial model

--- StudentSite/models.py
+++ StudentSite/models.py
@@ -1,2 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Task(models.Model):
+    str_repr = models.CharField(max_length=255)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User)
+    tasks = models.ManyToManyField(Task, through='StudentTaskRel')
+
+
+class StudentTaskRel(models.Model):
+    task = models.ForeignKey(Task)
+    student = models.ForeignKey(Student)
+    isTestTask = models.BooleanField()
+    isCompleted = models.BooleanField(default=False)
+    numberOfAttempts = models.IntegerField(default=0)
+    dateStarted = models.DateField(auto_created=True)
+    dateCompleted = models.DateField(null=True)
