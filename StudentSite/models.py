eb6a430e43a08d1a9a5964dcef65ce6b8ca6b7db
IlyaKulebyakin <kuler29@me.com>
2015-05-01 13:31:41 +0300

    models refactoring, full table and attributes check, relation_type methods,

--- StudentSite/models.py
+++ StudentSite/models.py
@@ -3,20 +3,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Task(models.Model):
+class TaskModel(models.Model):
     str_repr = models.CharField(max_length=255)
     difficulty = models.IntegerField(default=0)
 
 
-class Student(models.Model):
+class StudentModel(models.Model):
     user = models.OneToOneField(User)
     website = models.URLField(null=True)
-    tasks = models.ManyToManyField(Task, through='StudentTaskRel')
+    tasks = models.ManyToManyField(TaskModel, through='StudentTaskRel')
 
 
 class StudentTaskRel(models.Model):
-    task = models.ForeignKey(Task)
-    student = models.ForeignKey(Student)
+    task = models.ForeignKey(TaskModel)
+    student = models.ForeignKey(StudentModel)
     isTestTask = models.BooleanField()
     partial_solve = models.CharField(max_length=255, default='')
     isCompleted = models.BooleanField(default=False)
--- StudentSite/views.py
+++ StudentSite/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
-from .models import Student, Task, StudentTaskRel
+from .models import StudentModel, TaskModel, StudentTaskRel
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from .MathBackend import Task as Task_Backend
 from django.views.decorators.http import require_POST
+from .MathBackend.relation_type import OrderType
 
 
 def index_view(request):
@@ -14,7 +15,7 @@ def index_view(request):
 
 def test_view(request):
     user = request.user
-    task_rel = Student.objects.get(user=user).studenttaskrel_set.get(isTestTask=True)
+    task_rel = StudentModel.objects.get(user=user).studenttaskrel_set.get(isTestTask=True)
     task_obj = Task_Backend.from_string(task_rel.task.str_repr)
     response = render(request, 'StudentSite/test_base.html', {'task': task_obj, 'task_id': task_rel.id})
     response.set_cookie('partial_solve', task_rel.partial_solve)
@@ -24,25 +25,29 @@ def test_view(request):
 @require_POST
 def check_test_task(request):
     task_id = request.POST['task_id']
-    task = Task.objects.get(id=task_id)
+    task = TaskModel.objects.get(id=task_id)
     task_obj = Task_Backend.from_string(task.str_repr)
-    checkboxes_array = [("reflexivity", "reflexive" if task_obj.is_reflexive() else "non-reflexive"),
-                        ("anti-reflexivity", "reflexive" if task_obj.is_antireflexive() else "non-reflexive"),
-                        ("symmetry", "reflexive" if task_obj.is_symmetric() else "non-reflexive"),
-                        ("asymmetry", "reflexive" if task_obj.is_asymmetric() else "non-reflexive"),
-                        ("antisymmetry", "reflexive" if task_obj.is_antisymmetric() else "non-reflexive"),
-                        ("transitivity", "reflexive" if task_obj.is_transitive() else "non-reflexive"),
-                        ("equivalency", "reflexive" if task_obj.is_of_equivalence() else "non-reflexive"),
-                        ("order", "reflexive" if task_obj.is_of_order() else "non-reflexive"),
-                        ("order-strict", "reflexive" if task_obj.is_reflexive() else "non-reflexive"),
-                        ("order-linearity", "reflexive" if task_obj.is_reflexive() else "non-reflexive"),
-                        ]
+    checkboxes_array = [
+        ("reflexivity", "reflexive" if task_obj.is_reflexive() else "non-reflexive"),
+        ("anti-reflexivity", "anti-reflexive" if task_obj.is_antireflexive() else "non-anti-reflexive"),
+        ("symmetry", "symmetric" if task_obj.is_symmetric() else "non-symmetric"),
+        ("asymmetry", "asymmetric" if task_obj.is_asymmetric() else "non-asymmetric"),
+        ("antisymmetry", "antisymmetric" if task_obj.is_antisymmetric() else "non-antisymmetric"),
+        ("transitivity", "transitive" if task_obj.is_transitive() else "non-transitive"),
+        ("equivalency", "equivalent" if task_obj.is_of_equivalence() else "non-equivalent"),
+        ("order", "of-order" if task_obj.is_of_order() != OrderType.not_of_order else "not-of-order"),
+        ("order-strict", "strict" if task_obj.is_of_order().is_strict() else "not-strict"),
+        ("order-linearity", "linear" if task_obj.is_of_order().is_partial() else "partial")
+    ]
+
     result = True
-    checkboxes_saved_result=""
+    checkboxes_saved_result = ""
     for (check_name, check_answer) in checkboxes_array:
         if check_name in request.POST:
             answer = request.POST[check_name]
             result = result and (answer == check_answer)
+            if not result:
+                print(answer)
             checkboxes_saved_result = '$'.join([checkboxes_saved_result, "%s=%s" % (check_name, answer)])
     checkboxes_saved_result = checkboxes_saved_result[1:]
 
@@ -63,8 +68,7 @@ def check_test_task(request):
         ar_solve[i] = ' '.join(['+' if elem else '-' for elem in ar_solve[i]])
     ar_solve = '$'.join(ar_solve)
     ar_solve = '@'.join([ar_solve, checkboxes_saved_result])
-    print(ar_solve)
-    student = request.user.student
+    student = request.user.studentmodel
     rel = task.studenttaskrel_set.get(student=student)
     rel.partial_solve = ar_solve
     rel.save()
@@ -78,12 +82,10 @@ def logout_view(request):
 
 
 def login_view(request):
-    print(request.POST)
     username = request.POST["username"]
     password = request.POST["password"]
     user = authenticate(username=username, password=password)
     if user is not None:
         if user.is_active:
             login(request, user)
-            print(user)
     return HttpResponseRedirect(reverse('student_site:index_view'))
